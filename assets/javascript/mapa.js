a4423662904c1b19e86e07266963a73ded3d6327
LeonardoCastro <lcastrogonzalez@turing.ac.uk>
2020-03-24 16:18:33 +0000

    commit before merge

--- assets/javascript/mapa.js
+++ assets/javascript/mapa.js
@@ -1,42 +1,72 @@
-// The svg
-var mapSvg = d3.select("svg"),
-  width = +mapSvg.attr("width"),
-  height = +mapSvg.attr("height");
+//set dimensions
+ var urlTotal="https://raw.githubusercontent.com/LeonardoCastro/COVID19-Mexico/master/data/series_tiempo/covid19_mex_casos_totales.csv",
+     urlRecu="https://raw.githubusercontent.com/LeonardoCastro/COVID19-Mexico/master/data/series_tiempo/covid19_mex_recuperados.csv",
+     urlActivos="https://raw.githubusercontent.com/LeonardoCastro/COVID19-Mexico/master/data/series_tiempo/covid19_mex_casos_activos.csv",
+     urlMuertes="https://raw.githubusercontent.com/LeonardoCastro/COVID19-Mexico/master/data/series_tiempo/covid19_mex_muertes.csv",
+ w = 700,
+ h = 400;
+/*var  adjust = window.innerWidth;
+$('#mapa').scrollLeft(adjust/2);*/
 
-// Map and projection
-var path = d3.geoPath();
+var hover = function(d) {
+var div = document.getElementById('tooltip');
+    div.innerHTML = d.properties.name;
+var selector =div.innerHTML ;
+d3.csv(urlTotal,function(data) {
+   var largo = data.length;
+   var tope =largo-1;
+var tot = document.getElementById('col_tot');
+    tot.innerHTML = data[tope][div.innerHTML];
+});
+d3.csv(urlRecu,function(data) {
+   var largo = data.length;
+   var tope =largo-1;
+var recu = document.getElementById('col_recu');
+    recu.innerHTML = data[tope][div.innerHTML];
+});
+d3.csv(urlActivos,function(data) {
+   var largo = data.length;
+   var tope =largo-1;
+var recu = document.getElementById('col_act');
+    recu.innerHTML = data[tope][div.innerHTML];
+});
+d3.csv(urlMuertes,function(data) {
+   var largo = data.length;
+   var tope =largo-1;
+var recu = document.getElementById('col_muertes');
+    recu.innerHTML = data[tope][div.innerHTML];
+});
+   /* console.log(selector);
+var prueba = new Date(2020,2,21);
+d3.csv("https://raw.githubusercontent.com/LeonardoCastro/COVID19-Mexico/master/data/series_tiempo/covid19_mex_casos_totales.csv")
+  .row(function(d) { console.log({selector: d.prueba, value: +d.val}); }) //return {key: d.key, value: +d.value};
+  .get(function(error, rows) {console.log(rows); });*/
+};
+
+//define projection
 var projection = d3.geoMercator()
-  .scale(70)
-  .center([0,0])
-  .translate([width / 2, height / 2]);
+                   .center([-100, 22])
+                   .translate([ w/1.7, h/1.7])
+                   .scale([ w/.7 ]);
 
-// Data and color scale
-var data = d3.map();
-var colorScale = d3.scaleThreshold()
-  .domain([100000, 1000000, 10000000, 30000000, 100000000, 500000000])
-  .range(d3.schemeBlues[7]);
+//define path generator
+var path = d3.geoPath()
+             .projection(projection)
 
-// Load external data and boot
-d3.queue()
-  .defer(d3.json, "https://raw.githubusercontent.com/holtzy/D3-graph-gallery/master/DATA/world.geojson")
-  .defer(d3.csv, "https://raw.githubusercontent.com/holtzy/D3-graph-gallery/master/DATA/world_population.csv", function(d) { data.set(d.code, +d.pop); })
-  .await(ready);
+//svg
+var mapSvg = d3.select("#mapa")
+               .append("svg")
+                .attr("width", w)
+                .attr("height", h);
 
-function ready(error, topo) {
+//load GeoJson data
+d3.json("https://raw.githubusercontent.com/vidaleando/COVID-19/master/assets/javascript/mexico.json", function(json) {
+            // bind data
+  mapSvg.selectAll("path")
+        .data(json.features)
+        .enter()
+        .append("path")
+          .attr("d", path)
+          .on("mouseover", hover);
 
-  // Draw the map
-  mapSvg.append("g")
-    .selectAll("path")
-    .data(topo.features)
-    .enter()
-    .append("path")
-      // draw each country
-      .attr("d", d3.geoPath()
-        .projection(projection)
-      )
-      // set the color of each country
-      .attr("fill", function (d) {
-        d.total = data.get(d.id) || 0;
-        return colorScale(d.total);
-      });
-    }
+});
